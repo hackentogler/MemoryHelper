722c85bd7e81d9985b2341defcc84afa3f809efd
Jeffrey Hougentogler <jeff.hougentogler@gmail.com>
2025-01-25 13:36:40 -0500

    Initial Commit

---
+++ MemoryHelper/MemoryHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryHelperApp.swift
+//  MemoryHelper
+//
+//  Created by Jeffrey Hougentogler on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryHelperTests/MemoryHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryHelperTests.swift
+//  MemoryHelperTests
+//
+//  Created by Jeffrey Hougentogler on 1/25/25.
+//
+
+import Testing
+@testable import MemoryHelper
+
+struct MemoryHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
