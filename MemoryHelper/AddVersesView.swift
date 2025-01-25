166df424d5bddb3c5dc227f2432e097b72822e33
Jeffrey Hougentogler <jeff.hougentogler@gmail.com>
2025-01-25 18:36:42 -0500

    Added layout for AddVersesView

--- MemoryHelper/AddVersesView.swift
+++ MemoryHelper/AddVersesView.swift
@@ -8,8 +8,71 @@
 import SwiftUI
 
 struct AddVersesView: View {
+    let columns = [
+        GridItem(.flexible(minimum: 150), spacing: 20)
+    ]
+    
+    let booksOfTheBible = [
+            "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy",
+            "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel",
+            "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra",
+            "Nehemiah", "Esther", "Job", "Psalms", "Proverbs",
+            "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations",
+            "Ezekiel", "Daniel", "Hosea", "Joel", "Amos",
+            "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk",
+            "Zephaniah", "Haggai", "Zechariah", "Malachi",
+            "Matthew", "Mark", "Luke", "John", "Acts",
+            "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians",
+            "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy",
+            "2 Timothy", "Titus", "Philemon", "Hebrews", "James",
+            "1 Peter", "2 Peter", "1 John", "2 John", "3 John",
+            "Jude", "Revelation"
+        ]
+    
+    @State private var searchText = ""
+    
+    var filteredBooks: [String] {
+        if searchText.isEmpty {
+            return booksOfTheBible
+        } else {
+            return booksOfTheBible.filter { $0.lowercased().contains(searchText.lowercased()) }
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            //ScrollView {
+                VStack {
+                    Button(action: {
+                        print("Action 1 tapped")
+                    }){
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                            Text("Verse Finder")
+                        }.padding(10)
+                    }.buttonStyle(.borderedProminent)
+                    .padding(10)
+                    
+                    // Search TextField
+                    TextField("Search books of the Bible", text: $searchText)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                        .padding()
+                    
+                    // Filtered list of books
+                    List(filteredBooks, id: \.self) { book in
+                        Text(book)
+                            .font(.headline)
+                            .padding()
+                    }
+                
+                    
+                }
+                .navigationTitle("Add Verses")
+                .padding()
+            //}
+        }
     }
 }
 
